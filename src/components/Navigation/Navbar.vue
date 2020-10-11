8c82742bb39e6b4a0014e45fc6fd134930936964
Avarayr <mikamika83@gmail.com>
2020-10-11 15:25:22 -0700

    removed duplicate data keys (already exist in props)

--- src/components/Navigation/Navbar.vue
+++ src/components/Navigation/Navbar.vue
@@ -1,104 +1,96 @@
 <template>
+    <nav class="navbar navbar-expand-lg py-4 my-navbar fixed-top" id="header">
+        <i
+            class="fa fa-long-arrow-left  pl-3"
+            id="back-arrow"
+            aria-hidden="true"
+            @click="back"
+            v-if="hide_nav_brand"
+        ></i>
 
-  <nav class="navbar navbar-expand-lg py-4 my-navbar fixed-top" id="header">
+        <i class="pl-md-5 dynamic-brand" v-if="hide_nav_brand">
+            {{ title }}
+        </i>
 
-    <i class="fa fa-long-arrow-left  pl-3" id="back-arrow" aria-hidden="true" @click="back" v-if="hide_nav_brand">
-    </i>
-       
-    <i class="pl-md-5 dynamic-brand" v-if="hide_nav_brand">
-      {{ title }}
-    </i>
-
-    
-    <router-link :to="{ name: 'home' }" class="nunito dynamic-brand" v-if="!hide_nav_brand">
-      <i class="fa fa-graduation-cap" aria-hidden="true">
-        Admittedly
-      </i>
-    </router-link>
-      
-  </nav>
-
+        <router-link
+            :to="{ name: 'home' }"
+            class="nunito dynamic-brand"
+            v-if="!hide_nav_brand"
+        >
+            <i class="fa fa-graduation-cap" aria-hidden="true">
+                Admittedly
+            </i>
+        </router-link>
+    </nav>
 </template>
 
-
 <script>
- 
-  export default {
+export default {
     name: "NavbarComponent",
 
     props: {
-      hide_nav_brand: {
-        type: Boolean,
-        required: true,
-      },
+        hide_nav_brand: {
+            type: Boolean,
+            required: true
+        },
 
-      title: {
-        type: String,
-        required: false,
-      },
+        title: {
+            type: String,
+            required: false
+        }
     },
 
     data() {
-      return {
-        
-        hide_nav_brand: this.hide_nav_brand,
-        title: this.title,
-      
-      };
+        return {};
     },
 
     methods: {
-
-			back () {
-				this.$router.back()
-			}
-  
+        back() {
+            this.$router.back();
+        }
     }
-
-  };
+};
 </script>
 
 <style scoped>
-
-  a {
+a {
     text-decoration: none;
     color: white !important;
-  }
+}
 
-  .my-navbar {
+.my-navbar {
     font-size: 1.4rem;
     background-color: white !important;
-    color: #10D48E !important;
-  }
+    color: #10d48e !important;
+}
 
-  .navbar-brand {
+.navbar-brand {
     font-weight: bold;
     font-size: 3rem;
-    color: #10D48E !important;
-  }
+    color: #10d48e !important;
+}
 
-  fa-graduation-cap{
+fa-graduation-cap {
     font-size: 4rem;
     color: white !important;
-  }
+}
 
-  /* .navbar-brand
+/* .navbar-brand
   {
         position: absolute;
         left: 50%;
   } */
 
-  #back-arrow{
-		font-size: 2rem;	
+#back-arrow {
+    font-size: 2rem;
     color: white;
-  }
-  
-  #header.active {
-     box-shadow: 0 0 10px rgba(0,0,0,0.4);  
-     background-color: #10D48E !important;
-     color: white !important;
-     font-size: 3rem;
-     font-weight: bold;
 }
 
+#header.active {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+    background-color: #10d48e !important;
+    color: white !important;
+    font-size: 3rem;
+    font-weight: bold;
+}
 </style>
