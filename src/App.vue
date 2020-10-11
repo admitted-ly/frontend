6df827a23ed516f42ab43dade48f3b54a1a7f642
Avarayr <mikamika83@gmail.com>
2020-10-11 15:22:55 -0700

    added prettier settings in package.json

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,41 @@
 <template>
+    <div id="app">
+        <NavbarComponent :hide_nav_brand="hide_nav_brand" />
+        <router-view />
 
-  <div id="app">
-
-    <NavbarComponent :hide_nav_brand="hide_nav_brand"/>
-    <router-view />
-
-    <FooterComponent />
-
-  </div>
-
+        <FooterComponent />
+    </div>
 </template>
 
 <script>
-
 import NavbarComponent from "@/components/Navigation/Navbar.vue";
-import  FooterComponent from "@/components/Navigation/Footer.vue";
+import FooterComponent from "@/components/Navigation/Footer.vue";
 
 // import { apiService } from "@/common/api.service.js";
 
 export default {
+    name: "App",
+    components: {
+        NavbarComponent,
+        FooterComponent
+    },
 
-  name: "App",
-  components: {
-    NavbarComponent, 
-    FooterComponent
-  },
-
-  data(){
-    return{
-      hide_nav_brand: false,
-      title: null
-      
+    data() {
+        return {
+            hide_nav_brand: false,
+            title: null
+        };
     }
-  },
-
-  watch: {
-  	'$route': function(value) {
-			
-        if(value.meta.hideBrand) {
-          this.hide_nav_brand = true
-          this.title = value.meta.title
-        }else{
-          this.hide_nav_brand = false
-          this.title = null
-        }
-
-    }
-  }
 };
-
 </script>
 
 <style>
-
-  #nav a {
+#nav a {
     font-weight: bold;
     color: #2c3e50;
-  }
+}
 
-  #nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
     color: #42b983;
-  }
-
+}
 </style>
